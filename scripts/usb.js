1161ddb7902e3dcc99b72752c5e4549f534b298f
Minarox <contact@minarox.fr>
2023-06-13 10:25:18 +0200

    Add prevent default message if needed

--- scripts/usb.js
+++ scripts/usb.js
@@ -2,6 +2,7 @@ const { spawn } = require('child_process');
 // Fetch Elgato usb device id with command: lsusb | grep 'Elgato'
 const usbPath = `/sys/bus/usb/devices/8-1/authorized`;
 const command = process.argv[2];
+let prevent = false;
 
 function toggle(boolean) {
     spawn('echo', [parseInt(boolean), '>', usbPath]);
@@ -17,13 +18,12 @@ switch (command) {
     // TODO: USB Status
     case 'on':
         toggle(true);
-        done();
         break;
     case 'off':
         toggle(false);
-        done();
         break;
     case 'restart':
+        prevent = true;
         toggle(false);
         setTimeout(() => {
             toggle(true);
@@ -34,3 +34,5 @@ switch (command) {
         console.log('Invalid command. Usage: node usb.js [on|off|restart]');
         break;
 }
+
+if (!prevent) done();
