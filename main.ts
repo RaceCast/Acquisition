9b81765704163181319c87010725df9b4e99dba5
Minarox <contact@minarox.fr>
2023-08-16 13:52:27 +0200

    Rewrite main script

--- main.ts
+++ main.ts
@@ -4,9 +4,12 @@ import { io } from "socket.io-client";
 
 config({ path: `${__dirname}/.env` });
 
-let connected: boolean = false;
-let firefox: boolean = false;
-let interval = null;
+// --- Initialization ---
+// Check if auth key is defined
+if (!process.env.AUTH_KEY) {
+  process.stderr.write("ERROR: No auth key defined in .env file");
+  process.exit(1);
+}
 
 // Init socket.io
 const socket = io(`https://rallye.minarox.fr?key=${process.env.AUTH_KEY}`, {
@@ -16,46 +19,122 @@ const socket = io(`https://rallye.minarox.fr?key=${process.env.AUTH_KEY}`, {
   retries: Infinity,
 });
 
-socket.on("connect", () => {
-  console.log(`Connected to server (${new Date().toLocaleString()})`);
-  connected = true;
+// Define interfaces
+interface State {
+  online: boolean;
+  mpu6050: boolean;
+}
 
-  if (!firefox) {
-    firefox = true;
-    spawn("node", ["scripts/webrtc.js", "restart"]);
+interface Mpu6050 {
+  gyro: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  accel: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  rotation: {
+    x: number;
+    y: number;
+  };
+  temp: number;
+}
+
+// Define global variables
+// let firefox: boolean = false;
+const state: State = {
+  online: false,
+  mpu6050: false,
+};
+
+// --- Interface and events ---
+// Update console status
+function updateConsoleStatus(erase: boolean = true): void {
+  if (erase) {
+    for (let i = 0; i <= 2; i++) {
+      const y = i === 0 ? null : -1;
+      process.stdout.moveCursor(0, y);
+      process.stdout.clearLine(1);
+    }
+    process.stdout.cursorTo(0);
   }
+  process.stdout.write(
+    `Back-End: ${
+      state.online
+        ? `\x1b[32mOnline\x1b[89m\x1b[0m`
+        : `\x1b[31mOffline\x1b[89m\x1b[0m`
+    }\r\n`,
+  );
+  process.stdout.write(
+    `MPU6050: ${
+      state.mpu6050
+        ? `\x1b[32mOnline\x1b[89m\x1b[0m`
+        : `\x1b[31mOffline\x1b[89m\x1b[0m`
+    }\r\n`,
+  );
+}
 
-  // Send timestamp to server
-  interval = setInterval(() => {
-    if (connected) socket.volatile.emit("latency", Date.now());
-  }, 500);
-});
+// Socket.io events
+socket.on("connect", () => {
+  state.online = true;
+  updateConsoleStatus();
 
-socket.on("disconnect", () => {
-  console.log(`Disconnected from server (${new Date().toLocaleString()})`);
-  connected = false;
-  clearInterval(interval);
-});
+  // Run scripts
+  setTimeout(() => {
+    if (!state.mpu6050) runMpu6050();
+  }, 300);
 
-function runSensor() {
-  // Exécution du script
-  const sensor = spawn("node", ["scripts/sensor.js"], { stdio: "pipe" });
-
-  // Capturer la sortie du script
-  sensor.stdout.on("data", (data) => {
-    const values = JSON.parse(data.toString().trim());
-    if (connected) socket.volatile.emit("mpu6050", values);
+  socket.on("disconnect", () => {
+    state.online = false;
+    updateConsoleStatus();
   });
 
-  // Gestion de la fin du processus
-  sensor.on("exit", () => {
-    setTimeout(() => {
-      runSensor();
-    }, 1000);
+  socket.on("latency", (callback) => {
+    callback(Date.now());
+  });
+
+  socket.emit("state", state);
+
+  // if (!firefox) {
+  //    firefox = true;
+  //   spawn("node", ["scripts/webrtc.js", "restart"]);
+  // }
+});
+
+// Script execution
+function runMpu6050(): void {
+  const sensor = spawn("node", ["scripts/mpu6050.js"], { stdio: "pipe" });
+
+  // Update status when the script is ready
+  sensor.on("spawn", () => {
+    state.mpu6050 = true;
+    updateConsoleStatus();
+    socket.volatile.emit("state", state);
+  });
+
+  // Send data to the backend
+  sensor.stdout.on("data", (data: string) => {
+    const values: Mpu6050 = JSON.parse(data.toString().trim());
+    if (state.online) socket.volatile.emit("mpu6050", values);
+  });
+
+  // Restart the script if it crashes or exits
+  ["exit", "error"].forEach((type: string) => {
+    sensor.on(type, () => {
+      state.mpu6050 = false;
+      updateConsoleStatus();
+      socket.volatile.emit("state", state);
+      setTimeout(() => {
+        runMpu6050();
+      }, 2000);
+    });
   });
 }
 
-function runRouter() {
+/*function runRouter() {
   // Exécution du script
   const router = spawn("node", ["scripts/gps.js"], { stdio: "pipe" });
 
@@ -71,7 +150,7 @@ function runRouter() {
       runRouter();
     }, 1000);
   });
-}
+}*/
 
 /*function runStream() {
     // Exécution du script
@@ -91,6 +170,8 @@ function runRouter() {
     });
 }*/
 
-runSensor();
-runRouter();
+updateConsoleStatus(false);
+
+// restartSensor();
+// runRouter();
 // runStream();
---
+++ scripts/mpu6050.ts
@@ -0,0 +1,55 @@
+import i2c from "i2c-bus";
+import Mpu6050 from "i2c-mpu6050";
+
+// Define interface
+interface Data {
+  gyro: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  accel: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  rotation: {
+    x: number;
+    y: number;
+  };
+  temp: number;
+}
+
+// Define global variables
+const address: number = 0x68;
+const bus: number = i2c.openSync(1);
+const mpu6050 = new Mpu6050(bus, address);
+
+// Limit all values to 2 decimals
+function limitDecimals(data: Data): void {
+  for (const key in data) {
+    if (typeof data[key] === "object") limitDecimals(data[key]);
+    else data[key] = parseFloat(data[key].toFixed(2));
+  }
+}
+
+// Read sensor data
+function readData(): void {
+  // Read sensor data
+  const data: Data = mpu6050.readSync();
+
+  // Offset calibration
+  data.accel.x -= 0.058978759765625;
+  data.accel.y -= 0.0088987060546875;
+  data.accel.z -= 0.059643090820312494;
+  data.gyro.x -= -0.7022061068702323;
+  data.gyro.y -= 1.0760305343511471;
+  data.rotation.x -= -0.4804232806148877;
+  data.rotation.y -= -3.1856752923673435;
+
+  // Show data
+  limitDecimals(data);
+  process.stdout.write(JSON.stringify(data));
+}
+
+setInterval(readData, 80);
