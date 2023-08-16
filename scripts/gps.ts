19c108c73450d99655b8e26afa35cd0ff3c5a817
Minarox <contact@minarox.fr>
2023-08-16 21:22:09 +0200

    Rewrite GPS script

--- scripts/gps.ts
+++ scripts/gps.ts
@@ -1,75 +1,99 @@
-import { spawn } from "child_process";
+import { exec, ExecException } from "child_process";
 
-let busy: boolean = false;
-let interval = null;
+let skip: boolean = true;
 
-function sendATCommand(command) {
-  return new Promise((resolve) => {
-    if (busy) return resolve(false);
-    busy = true;
-    const socat = spawn("socat", ["- /dev/ttyUSB2"], { shell: false });
-    let data = "";
+function checkState(): void {
+  skip = true;
 
-    socat.stdout.on("data", (output) => {
-      data += output.toString();
-    });
-
-    socat.on("close", () => {
-      data = data.trim();
-
-      // Le module renvoie une erreur
-      if (data.startsWith("+CME ERROR: 505")) {
-        clearInterval(interval);
-        setTimeout(() => {
-          start();
-        }, 1500);
-      }
-
-      // Analyse de la réponse GPS du module EC25
-      if (data.startsWith("+QGPSLOC:")) {
-        const dataElements = data.split(":")[1].split(",");
-        const latitude =
-          parseFloat(dataElements[1].slice(0, 2)) +
-          parseFloat(dataElements[1].slice(2)) / 60;
-        const longitude =
-          parseFloat(dataElements[3].slice(0, 3)) +
-          parseFloat(dataElements[3].slice(3)) / 60;
-
-        if (latitude && longitude) {
-          console.log(
-            JSON.stringify({
-              latitude: latitude,
-              longitude: longitude,
-              altitude: parseFloat(dataElements[6]),
-              speed: parseFloat(dataElements[9]),
-            }),
+  // Check GPS state
+  exec(
+    "sudo mmcli -m 0 --command='AT+QGPS?'",
+    (error: ExecException, stdout: string): void => {
+      if (error) {
+        // Relaunch the script
+        setTimeout(checkState, 1000);
+      } else if (stdout.trim().startsWith("response: '+QGPS: 1'")) {
+        // Disable GPS
+        setTimeout((): void => {
+          exec(
+            "sudo mmcli -m 0 --command='AT+QGPSEND'",
+            (error: ExecException): void => {
+              if (error) {
+                // Relaunch the script
+                setTimeout(checkState, 1000);
+              } else {
+                // Init GPS
+                setTimeout(initGPS, 1000);
+              }
+            },
           );
+        }, 1000);
+      } else {
+        // Init GPS
+        setTimeout(initGPS, 1000);
+      }
+    },
+  );
+}
+
+function initGPS(): void {
+  // Set GPS format
+  exec(
+    "sudo mmcli -m 0 --command='AT+QGPSCFG=\"nmeasrc\",1'",
+    (error: ExecException): void => {
+      if (error) {
+        // Relaunch the script
+        setTimeout(checkState, 1000);
+      } else {
+        // Enable GPS
+        setTimeout((): void => {
+          exec(
+            "sudo mmcli -m 0 --command='AT+QGPS=1'",
+            (error: ExecException): void => {
+              if (error) {
+                // Relaunch the script
+                setTimeout(checkState, 1000);
+              } else {
+                // Get GPS location
+                setInterval(getPosition, 1000);
+              }
+            },
+          );
+        }, 1000);
+      }
+    },
+  );
+}
+
+function getPosition(): void {
+  // Get GPS location
+  exec(
+    "sudo mmcli -m 0 --command='AT+QGPSLOC=1'",
+    (_error: ExecException, stdout: string): void => {
+      if (skip) {
+        skip = false;
+      } else {
+        if (stdout.trim().startsWith("response: '+QGPSLOC:")) {
+          const data: string[] = stdout.trim().split(":")[2].trim().split(",");
+          const latitude: number =
+            parseFloat(data[1].slice(0, 2)) + parseFloat(data[1].slice(2)) / 60;
+          const longitude: number =
+            parseFloat(data[3].slice(0, 3)) + parseFloat(data[3].slice(3)) / 60;
+
+          if (latitude && longitude) {
+            process.stdout.write(
+              JSON.stringify({
+                latitude: latitude,
+                longitude: longitude,
+                altitude: parseFloat(data[6]),
+                speed: parseFloat(data[9]),
+              }),
+            );
+          }
         }
       }
-
-      busy = false;
-      socat.kill(0);
-      return resolve(data);
-    });
-
-    socat.stdin.write(`${command}\r\n`);
-    socat.stdin.end();
-  });
+    },
+  );
 }
 
-function start() {
-  sendATCommand("AT+QGPSEND").then(() => {
-    sendATCommand("AT+QGPS=1").then(() => {
-      sendATCommand('AT+QGPSCFG="nmeasrc",1').then(() => {
-        sendATCommand('AT+QGPSGNMEA="GGA",1').then(() => {
-          sendATCommand("AT+QGPSLOC=1");
-          interval = setInterval(() => {
-            sendATCommand("AT+QGPSLOC=1");
-          }, 1000);
-        });
-      });
-    });
-  });
-}
-
-start();
+checkState();
