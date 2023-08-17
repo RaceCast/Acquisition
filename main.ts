3a9383aa5499b39c69a0c1427ba10f2ca8b5f324
Minarox <contact@minarox.fr>
2023-08-17 13:32:24 +0200

    Add cleanup function

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 import { config } from "dotenv";
-import { ChildProcessWithoutNullStreams, spawn } from "node:child_process";
+import {
+  ChildProcessWithoutNullStreams,
+  exec,
+  spawn,
+} from "node:child_process";
 import { io, Socket } from "socket.io-client";
 
 config({ path: `${__dirname}/.env` });
@@ -55,6 +59,13 @@ interface Gps {
   speed: number;
 }
 
+interface Processes {
+  exit: boolean;
+  mpu6050: ChildProcessWithoutNullStreams | null;
+  gps: ChildProcessWithoutNullStreams | null;
+  webrtc: ChildProcessWithoutNullStreams | null;
+}
+
 // Define global variables
 const state: State = {
   online: false,
@@ -62,19 +73,28 @@ const state: State = {
   gps: false,
   webrtc: false,
 };
+const processes: Processes = {
+  exit: false,
+  mpu6050: null,
+  gps: null,
+  webrtc: null,
+};
 
 // --- Interface and events ---
+// Clear console
+function clearConsole(): void {
+  for (let i: number = 0; i <= 4; i++) {
+    const y: number | null = i === 0 ? null : -1;
+    process.stdout.moveCursor(0, y);
+    process.stdout.clearLine(1);
+  }
+  process.stdout.cursorTo(0);
+}
+
 // Update console status
 function updateConsoleStatus(erase: boolean = true): void {
-  if (process.stdout.isTTY) {
-    if (erase) {
-      for (let i: number = 0; i <= 4; i++) {
-        const y: number | null = i === 0 ? null : -1;
-        process.stdout.moveCursor(0, y);
-        process.stdout.clearLine(1);
-      }
-      process.stdout.cursorTo(0);
-    }
+  if (process.stdout.isTTY || !processes.exit) {
+    if (erase) clearConsole();
     process.stdout.write(
       `Back-End: ${
         state.online
@@ -134,21 +154,17 @@ socket.on("connect", (): void => {
   socket.emit("state", state);
 });
 
-// Script execution
+// Run MPU6050 script
 function runMpu6050(): void {
   // Run the script
-  const process: ChildProcessWithoutNullStreams = spawn(
-    "node",
-    ["scripts/mpu6050.js"],
-    { stdio: "pipe" },
-  );
+  processes.mpu6050 = spawn("node", ["scripts/mpu6050.js"], { stdio: "pipe" });
 
   // Send data to the backend
-  process.stdout.on("data", (data: string): void => {
+  processes.mpu6050.stdout.on("data", (data: string): void => {
     if (!state.mpu6050) {
       state.mpu6050 = true;
       updateConsoleStatus();
-      socket.volatile.emit("state", state);
+      socket.emit("state", state);
     }
     const values: Mpu6050 = JSON.parse(data);
     if (state.online) socket.volatile.emit("mpu6050", values);
@@ -156,31 +172,31 @@ function runMpu6050(): void {
 
   // Restart the script if it crashes or exits
   ["exit", "error"].forEach((type: string): void => {
-    process.on(type, (): void => {
+    processes.mpu6050.on(type, (): void => {
       state.mpu6050 = false;
       updateConsoleStatus();
-      socket.volatile.emit("state", state);
-      setTimeout((): void => {
-        runMpu6050();
-      }, 2000);
+      socket.emit("state", state);
+      processes.mpu6050 = null;
+      if (!processes.exit) {
+        setTimeout((): void => {
+          runMpu6050();
+        }, 2000);
+      }
     });
   });
 }
 
+// Run GPS script
 function runGps(): void {
   // Run the script
-  const process: ChildProcessWithoutNullStreams = spawn(
-    "node",
-    ["scripts/gps.js"],
-    { stdio: "pipe" },
-  );
+  processes.gps = spawn("node", ["scripts/gps.js"], { stdio: "pipe" });
 
   // Send data to the backend
-  process.stdout.on("data", (data: string): void => {
+  processes.gps.stdout.on("data", (data: string): void => {
     if (!state.gps) {
       state.gps = true;
       updateConsoleStatus();
-      socket.volatile.emit("state", state);
+      socket.emit("state", state);
     }
     const values: Gps = JSON.parse(data);
     if (state.online) socket.volatile.emit("gps", values);
@@ -188,37 +204,74 @@ function runGps(): void {
 
   // Restart the script if it crashes or exits
   ["exit", "error"].forEach((type: string): void => {
-    process.on(type, (): void => {
+    processes.gps.on(type, (): void => {
       state.gps = false;
       updateConsoleStatus();
-      socket.volatile.emit("state", state);
-      setTimeout((): void => {
-        runGps();
-      }, 2000);
+      socket.emit("state", state);
+      processes.gps = null;
+      if (!processes.exit) {
+        setTimeout((): void => {
+          runGps();
+        }, 2000);
+      }
     });
   });
 }
 
+// Run WebRTC script
 function runWebrtc(): void {
   // Run the script
-  const process: ChildProcessWithoutNullStreams = spawn("node", [
-    "scripts/webrtc.js",
-    "restart",
-  ]);
+  processes.webrtc = spawn("node", ["scripts/webrtc.js", "restart"]);
 
-  process.on("spawn", (): void => {
+  processes.webrtc.on("spawn", (): void => {
     state.webrtc = true;
     updateConsoleStatus();
-    socket.volatile.emit("state", state);
+    socket.emit("state", state);
   });
 
   ["exit", "error"].forEach((type: string): void => {
-    process.on(type, (): void => {
+    processes.webrtc.on(type, (): void => {
       state.webrtc = false;
       updateConsoleStatus();
-      socket.volatile.emit("state", state);
+      processes.webrtc = null;
+      socket.emit("state", state);
     });
   });
 }
+
+// Cleanup
+function cleanup(): void {
+  processes.exit = true;
+
+  // Exit message
+  if (process.stdout.isTTY) {
+    clearConsole();
+    process.stdout.write("Clear processes and exiting...\r\n");
+  }
+
+  // Kill processes
+  if (processes.mpu6050) processes.mpu6050.kill();
+  if (processes.gps) {
+    processes.gps.kill();
+    exec("sudo mmcli -m 0 --command='AT+QGPSEND'");
+  }
+  if (processes.webrtc) {
+    processes.webrtc.kill();
+    spawn("node", ["scripts/webrtc.js", "stop"]);
+  }
+
+  // Disconnect socket
+  socket.disconnect();
+
+  // Exit
+  process.exit();
+}
+
+// Cleanup on exit
+["exit", "SIGINT", "SIGUSR1", "SIGUSR2", "uncaughtException"].forEach(
+  (type: string): void => {
+    process.on(type, cleanup);
+  },
+);
 
 updateConsoleStatus(false);
