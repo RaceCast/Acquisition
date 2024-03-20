7670aeef41d0d359ea68bca8cf9914e19f3a6379
Mathis Serrieres Maniecki <mathis.sema@gmail.com>
2024-03-20 15:49:07 +0100

    refactor: Move exec in utils

--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -1,20 +1,4 @@
-import {exec} from "child_process";
-import {ExecException} from "node:child_process";
-import {logMessage, LogType} from "../utils";
-
-/**
- * @function execWrapper
- * @description Wrapper for exec function
- * @param {string} command - Command to execute
- * @return {Promise<string>} output of the command
- */
-function execWrapper(command: string): Promise<string> {
-    return new Promise((resolve): void => {
-        exec(command, (_error: ExecException | null, stdout: string): void => {
-            setTimeout(() => resolve(stdout), 100);
-        });
-    });
-}
+import {execute, logMessage, LogType} from "../utils";
 
 /**
  * @function restartAudio
@@ -22,9 +6,9 @@ function execWrapper(command: string): Promise<string> {
  * @return {Promise<void>}
  */
 async function restartAudio(): Promise<void> {
-    await execWrapper('/etc/init.d/alsa-utils restart');
-    await execWrapper('systemctl --user restart wireplumber.service');
-    await execWrapper('systemctl --user restart pipewire.service');
+    await execute('/etc/init.d/alsa-utils restart');
+    await execute('systemctl --user restart wireplumber.service');
+    await execute('systemctl --user restart pipewire.service');
 }
 
 /**
@@ -33,8 +17,8 @@ async function restartAudio(): Promise<void> {
  * @return {Promise<void>}
  */
 async function setModemSettings(): Promise<void> {
-    const echoResponse: string = await execWrapper(`echo 'ATE0' | socat - /dev/ttyUSB2,crnl`);
-    const scanModeResponse: string = await execWrapper(`echo 'AT+QCFG="nwscanmode",0' | socat - /dev/ttyUSB2,crnl`);
+    const echoResponse: string = await execute(`echo 'ATE0' | socat - /dev/ttyUSB2,crnl`);
+    const scanModeResponse: string = await execute(`echo 'AT+QCFG="nwscanmode",0' | socat - /dev/ttyUSB2,crnl`);
 
     if (echoResponse.trim() !== 'OK' || scanModeResponse.trim() !== 'OK') {
         logMessage(`Unable to set modem settings. Exiting...`, LogType.ERROR, true);
@@ -48,7 +32,7 @@ async function setModemSettings(): Promise<void> {
  * @return {Promise<true>} true when the modem is connected
  */
 async function waitForConnection(): Promise<true> {
-    const connectedResponse: string = await execWrapper(`echo 'AT+CGATT?' | socat - /dev/ttyUSB2,crnl`);
+    const connectedResponse: string = await execute(`echo 'AT+CGATT?' | socat - /dev/ttyUSB2,crnl`);
     if (!connectedResponse.trim().startsWith('+CGATT: 1')) {
         await waitForConnection();
     }
