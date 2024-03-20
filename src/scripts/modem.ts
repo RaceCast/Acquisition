de50f06bddac2381aab708dda27446d7d51f1ed5
Mathis S.M <contact@minarox.fr>
2024-03-20 19:55:48 +0100

    refactor: Improve code

--- src/scripts/modem.ts
+++ src/scripts/modem.ts
@@ -1,5 +1,5 @@
 import {execute} from "../utils";
-import {Network, Signal, GPS} from "../types/modem";
+import {GPS, Network, Signal, Type} from "../types/modem";
 
 // Count data to switch between commands
 let data_count: number = 0;
@@ -8,16 +8,16 @@ let data_count: number = 0;
  * @function atCommand
  * @description Execute AT command and return response
  * @param {string} atCommand - AT commad to execute
- * @param {boolean} exit_on_error - Exit process on error 
+ * @param {boolean} exitOnError - Exit process on error
  * @returns {Promise<string>} modem response
  */
-async function atCommand(atCommand: string, exit_on_error: boolean = true): Promise<string> {
+async function atCommand(atCommand: string, exitOnError: boolean = true): Promise<string> {
     const command: string = `echo '${atCommand}' | socat - /dev/ttyUSB2,crnl`;
     return new Promise(async (resolve): Promise<void> => {
         await execute(command)
             .then((response) => resolve(response))
             .catch(() => {
-                if (exit_on_error) {
+                if (exitOnError) {
                     process.exit(1);
                 }
             });
@@ -32,41 +32,45 @@ async function atCommand(atCommand: string, exit_on_error: boolean = true): Prom
 async function getDatas(): Promise<void> {
     // Get network data
     if (data_count === 0) {
-        const network_response: string = await atCommand(`AT+QNWINFO`, false);
-        if (network_response.trim().startsWith("+QNWINFO:")) {
-            const data: Array<string> = network_response.trim().split(":")[1].trim().split(",");
+        const networkResponse: string = await atCommand(`AT+QNWINFO`, false);
+        if (networkResponse.trim().startsWith("+QNWINFO:")) {
+            const data: Array<string> = networkResponse.trim().split(":")[1].trim().split(",");
             const network: Network = {
                 access: data[0].slice(1, -1),
                 band: data[2].slice(1, -1),
-                channel: parseInt(data[3].slice(1, -6)) 
+                channel: parseInt(data[3].slice(1, -6))
             };
 
             process.stdout.write(
-                JSON.stringify({ Network: network })
+                JSON.stringify({Network: network})
             );
-        } else process.stderr.write("Network");
+        } else {
+            process.stderr.write(Type.NETWORK);
+        }
     }
 
     // Get signal data
     if (data_count === 3) {
-        const signal_response: string = await atCommand(`AT+CSQ`, false);
-        if (signal_response.trim().startsWith("+CSQ:")) {
-            const data: Array<string> = signal_response.trim().split(":")[1].trim().split(",");
+        const signalResponse: string = await atCommand(`AT+CSQ`, false);
+        if (signalResponse.trim().startsWith("+CSQ:")) {
+            const data: Array<string> = signalResponse.trim().split(":")[1].trim().split(",");
             const signal: Signal = {
                 signal: parseInt(data[0]),
                 error_rate: parseInt(data[1])
             };
 
             process.stdout.write(
-                JSON.stringify({ Signal: signal })
+                JSON.stringify({Signal: signal})
             );
-        } else process.stderr.write("Signal");
+        } else {
+            process.stderr.write(Type.SIGNAL);
+        }
     }
 
     // Get GPS data
-    const gps_response = await atCommand(`AT+QGPSLOC=1`, false)
-    if (gps_response.trim().startsWith("+QGPSLOC:")) {
-        const data = gps_response.trim().split(":")[1].trim().split(",");
+    const gpsResponse = await atCommand(`AT+QGPSLOC=1`, false);
+    if (gpsResponse.trim().startsWith("+QGPSLOC:")) {
+        const data = gpsResponse.trim().split(":")[1].trim().split(",");
         const gps: GPS = {
             latitude: parseFloat(data[1].slice(0, 2)) + parseFloat(data[1].slice(2)) / 60,
             longitude: parseFloat(data[3].slice(0, 3)) + parseFloat(data[3].slice(3)) / 60,
@@ -74,12 +78,16 @@ async function getDatas(): Promise<void> {
             speed: parseFloat(data[9]),
         };
         process.stdout.write(
-            JSON.stringify({ GPS: gps })
+            JSON.stringify({GPS: gps})
         );
-    } else process.stderr.write("GPS");
+    } else {
+        process.stderr.write(Type.GPS);
+    }
 
     data_count++;
-    if (data_count === 6) data_count = 0;
+    if (data_count === 6) {
+        data_count = 0;
+    }
 
     // Recursive call
     setTimeout(getDatas);
@@ -94,8 +102,8 @@ async function initGPS(): Promise<void> {
     // Set data format
     await atCommand(`AT+QGPSCFG=\"nmeasrc\",1`);
 
-    const gps_state: string = await atCommand(`AT+QGPS?`);
-    if (gps_state.trim().startsWith("+QGPS: 0")) {
+    const gpsState: string = await atCommand(`AT+QGPS?`);
+    if (gpsState.trim().startsWith("+QGPS: 0")) {
         // Enable GPS
         await atCommand(`AT+QGPS=1`);
 
@@ -110,9 +118,11 @@ async function initGPS(): Promise<void> {
 
 // Init communication with the modem
 /**
- * 
+ * @function start
+ * @description Start the modem script
+ * @returns {Promise<void>}
  */
-async function start() {
+async function start(): Promise<void> {
     // Disable duplicate echo
     await atCommand(`ATE0`);
 
@@ -120,4 +130,4 @@ async function start() {
     await initGPS();
 }
 
-start();
+start().then();
