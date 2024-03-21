5f33c3fc56a77162f45092ba356c889eab4fd1d1
Mathis S.M <contact@minarox.fr>
2024-03-21 21:09:30 +0100

    fix: Script processes

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,24 @@
 import {exec, ExecException} from "node:child_process";
-import {LogType} from "./types/global";
+import {LogLevel} from "./types/global";
 
 /**
  * Log message to the console
- * 
+ *
  * @param {string} message - Message to log
- * @param {LogType} type - Type of log message
- * @param {boolean} newLine - Add new line at the start
+ * @param {LogLevel} level - Level of log message
  * @returns {void}
  */
-export function logMessage(message: string, type: LogType = LogType.INFO, newLine: boolean = false): void {
+export function logMessage(message: string, level: LogLevel = LogLevel.INFO): void {
+    const type: string = level === LogLevel.ERROR ? 'stderr' : 'stdout';
+
     if (process[type].isTTY) {
-        process[type].write(`${newLine ? '\n' : ''}[${new Date().toLocaleTimeString('fr-FR')}] ${message}\n`);
+        process[type].write(`[${new Date().toLocaleTimeString('fr-FR')}] [${level}] ${message}\n`);
     }
 }
 
 /**
  * Execute command on the host
- * 
+ *
  * @param {string} command - Command to execute
  * @returns {Promise<string>} Result of the command
  */
@@ -34,7 +35,7 @@ export function execute(command: string): Promise<string> {
 
 /**
  * Execute AT command and return response
- * 
+ *
  * @param {string} command - AT command to execute
  * @returns {Promise<string>} Modem response
  */
@@ -44,15 +45,15 @@ export function executeAT(command: string): Promise<string> {
 
 /**
  * Wait a certain amount of time
- * 
- * @params {number} ms - Time to wait in milisecond
+ *
+ * @params {number} ms - Time to wait in millisecond
  * @returns {void}
  */
 export function wait(ms: number): void {
     const start: number = Date.now();
     let now: number = start;
-    
+
     while (now - start < ms) {
-      now = Date.now();
+        now = Date.now();
     }
 }
