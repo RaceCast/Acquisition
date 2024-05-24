157ab4d4702fbebe063483b6668b2a0bfe1d79c3
Mathis Serrieres Maniecki <mathis.sema@gmail.com>
2024-05-24 09:36:12 +0200

    feat: Add old scripts

---
+++ misc/sensor-calibration.ts
@@ -0,0 +1,84 @@
+import i2c from "i2c-bus";
+import Mpu6050 from "i2c-mpu6050";
+
+// Init I2C bus with MPU6050 address
+const address = 0x68;
+const i2c1 = i2c.openSync(1);
+
+// Init MPU6050
+const sensor = new Mpu6050(i2c1, address);
+
+// Variables pour la calibration des offsets des capteurs
+const calibrationIterations = 10000;
+const sumOffsets = {
+  accel: { x: 0, y: 0, z: 0 },
+  gyro: { x: 0, y: 0, z: 0 },
+  rotation: { x: 0, y: 0 },
+};
+let currentIteration = 0;
+
+// Launch calibration
+function calibrate() {
+  const data = sensor.readSync();
+  sumOffsets.accel.x += data.accel.x;
+  sumOffsets.accel.y += data.accel.y;
+  sumOffsets.accel.z += data.accel.z;
+  sumOffsets.gyro.x += data.gyro.x;
+  sumOffsets.gyro.y += data.gyro.y;
+  sumOffsets.gyro.z += data.gyro.z;
+  sumOffsets.rotation.x += data.rotation.x;
+  sumOffsets.rotation.y += data.rotation.y;
+  currentIteration++;
+
+  // Show iterations
+  console.clear();
+  console.log(
+    `Calibration en cours - Itération ${currentIteration}/${calibrationIterations}`,
+  );
+
+  // Stop script after 10000 iterations
+  if (currentIteration === calibrationIterations) {
+    clearInterval(calibrationInterval);
+    finishCalibration();
+  }
+}
+
+// Compute offsets
+function calculateOffsets() {
+  return {
+    accel: {
+      x: sumOffsets.accel.x / calibrationIterations,
+      y: sumOffsets.accel.y / calibrationIterations,
+      z: sumOffsets.accel.z / calibrationIterations,
+    },
+    gyro: {
+      x: sumOffsets.gyro.x / calibrationIterations,
+      y: sumOffsets.gyro.y / calibrationIterations,
+      z: sumOffsets.gyro.z / calibrationIterations,
+    },
+    rotation: {
+      x: sumOffsets.rotation.x / calibrationIterations,
+      y: sumOffsets.rotation.y / calibrationIterations,
+    },
+  };
+}
+
+// Show results
+function displayOffsets(offsets) {
+  // Remove gravitational acceleration from z-axis accelerometer value
+  offsets.accel.z -= 1;
+
+  console.clear();
+  console.log("Offsets:", offsets);
+}
+
+// Finish calibration
+function finishCalibration() {
+  const offsets = calculateOffsets();
+  displayOffsets(offsets);
+
+  process.exit();
+}
+
+// Start calibration
+const calibrationInterval = setInterval(calibrate, 100);
