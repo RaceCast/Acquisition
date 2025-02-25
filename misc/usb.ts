a57a9ccc6a66c1c2e98eecf49d77956c2a4eebe5
Mathis S.M <mathis.sema@gmail.com>
2025-02-25 15:49:40 +0100

    refactor: Indent spaces and types

--- misc/usb.ts
+++ misc/usb.ts
@@ -1,53 +1,57 @@
 import { spawn } from "child_process";
 import fs from "fs";
 
-const usbPath = `/sys/bus/usb/devices/6-1/authorized`;
-const command = process.argv[2];
-let prevent = false;
+const usbPath: string = `/sys/bus/usb/devices/6-1/authorized`;
+const command: string = process.argv[2] || '';
+let prevent: boolean = false;
 
-function toggle(boolean) {
-  fs.writeFile(usbPath, Number(boolean).toString(), () => {});
+function toggle(boolean: boolean): void {
+    fs.writeFile(usbPath, Number(boolean).toString(), () => {});
 }
 
-function done() {
-  setTimeout(() => {
-    console.log("done");
-  }, 1000);
+function done(): void {
+    setTimeout(() => {
+        console.log("done");
+    }, 1000);
 }
 
 switch (command) {
-  case "status":
-    prevent = true;
-    const lsusb = spawn("lsusb");
-    let data = "";
+    case "status":
+        prevent = true;
+        const lsusb = spawn("lsusb");
+        let data: string = "";
+        
+        lsusb.stdout.on("data", (output: string): void => {
+            data += output.toString();
+        });
+        
+        lsusb.on("close", (): void => {
+            if (data.trim().includes("Elgato")) console.log("connected");
+            else console.log("disconnected");
+        });
+        break;
 
-    lsusb.stdout.on("data", (output) => {
-      data += output.toString();
-    });
+    case "on":
+        toggle(true);
+        break;
 
-    lsusb.on("close", () => {
-      if (data.trim().includes("Elgato")) console.log("connected");
-      else console.log("disconnected");
-    });
-    break;
-  case "on":
-    toggle(true);
-    break;
-  case "off":
-    toggle(false);
-    break;
-  case "restart":
-    prevent = true;
-    toggle(false);
-    setTimeout(() => {
-      toggle(true);
-      done();
-    }, 1000);
-    break;
-  default:
-    prevent = true;
-    console.log("Invalid command. Usage: node usb.js [status|on|off|restart]");
-    break;
+    case "off":
+        toggle(false);
+        break;
+
+    case "restart":
+        prevent = true;
+        toggle(false);
+        setTimeout(() => {
+            toggle(true);
+            done();
+        }, 1000);
+        break;
+
+    default:
+        prevent = true;
+        console.log("Invalid command. Usage: node usb.js [status|on|off|restart]");
+        break;
 }
 
 if (!prevent) done();
